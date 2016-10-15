ef30f346f709acbb34b5eef39b42b3c827c6cafd
logosghost <jimkim777@gmail.com>
2016-10-15 15:16:16 -0400

    chore(setup webpack): add webpack-production.config.js
    add webpack-production.config.js, anticipating production builds

---
+++ app/logger.es6
@@ -0,0 +1,10 @@
+console.log('logger is loaded');
+const checkName = (firstName, lastName) => {
+  if (firstName !== 'nader' || lastName !== 'dabit') {
+    console.log('you are not nader dabit');
+
+  } else {
+    console.log('you are nader dabit');
+  }
+};
+checkName('nader', 'jackson');
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,29 @@ module.exports = {
     filename: "bundle.js",
     path: __dirname + "/dist",
   },
-  watch: true
+  watch: true,
+  module: {
+    preloaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'jshint-loader'
+      }
+    ],
+    loaders: [
+      {
+        test: /\.es6$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.es6']
+  },
 };
+// allows use of require('./logger') instead of require('./logger.js');
